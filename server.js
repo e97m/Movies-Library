10d50599e6d2646da344d1e7f1794c62098b4db1
e97m <majdalawi97@yahoo.com>
2022-01-31 20:58:37 +0200

    more pages added like trending and tv shows

--- server.js
+++ server.js
@@ -1,48 +1,162 @@
+'use strict';
+
+//require packages
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
+//declaring my port constant and the app
+const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 
-const movieData = require('./Movie Data/data.json');
-
+//creat websit pages
 app.get('/', movieDataHandler);
 app.get('/favorite', favorateHandler);
+app.get('/trending', trendingHandler)
+app.get('/search', searchHandler)
+app.get('/popular', popularHandler)
+app.get('/tv', tvDataHandler)
 app.get('*', notFoundHndler);
 app.get(serverErrorHndler);
 
 
-//construtor
+//construtor for main page
 function Movies(title, poster_path, overview) {
     this.title = title;
     this.poster_path = poster_path;
     this.overview = overview;
 }
 
+//constructor for trending and search pages
+function TrendMovies(id, title, release_date, poster_path, overview) {
+    this.id = id;
+    this.title = title;
+    this.release_date = release_date
+    this.poster_path = poster_path;
+    this.overview = overview;
+}
+
+//constructor for popular page
+function TrendMovies(id, original_title, release_date, poster_path, overview) {
+    this.id = id;
+    this.original_title = original_title;
+    this.release_date = release_date
+    this.poster_path = poster_path;
+    this.overview = overview;
+}
+
+//constructor for tv page
+function TvMovies(id, name, first_air_date, poster_path, overview) {
+    this.id = id;
+    this.name = name;
+    this.first_air_date = first_air_date
+    this.poster_path = poster_path;
+    this.overview = overview;
+}
+
+//the data on main page
+const movieData = require('./Movie Data/data.json');
 function movieDataHandler(req, res) {
     let listOfMovies = [];
-        let movie1 = new Movies(movieData.title, movieData.poster_path, movieData.overview)
-        listOfMovies.push(movie1)
-    console.log(listOfMovies)
+    let myMovie = new Movies(movieData.title, movieData.poster_path, movieData.overview)
+    listOfMovies.push(myMovie)
     return res.status(200).json(listOfMovies);
 }
 
+//favorate page
 function favorateHandler(req, res) {
     return res.status(200).send("Welcome to Favorite Page");
 }
 
-function notFoundHndler(req, res) {
-    return res.status(404).send("Not Found!!");
+// the data on trending page
+function trendingHandler(req, res) {
+    let listOfMovies = [];
+    // let url=`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`   *** did not work ***
+    let url = `https://api.themoviedb.org/3/trending/all/week?api_key=abb2fd3d9eb8c561dbf599a20e3ca490&language=en-US`
+    axios.get(url)
+        .then(
+            (result) => {
+                // console.log(result)   was done to know the imported arr
+                result.data.results.forEach(trendingMovies => {
+                    let myMovie = new TrendMovies(trendingMovies.id, trendingMovies.title, trendingMovies.release_date, trendingMovies.poster_path, trendingMovies.overview)
+                    listOfMovies.push(myMovie)
+                }
+                )
+                res.status(200).json(listOfMovies);
+            }
+        )
+        .catch((err) => { console.log(err) })
 }
 
-function serverErrorHndler(error, req, res) {
-    let err = {
-        status : 500,
-        message : "Sorry, something went wrong"
+//search page
+function searchHandler(req, res) {
+    let listOfMovies = [];
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=abb2fd3d9eb8c561dbf599a20e3ca490&language=en-US&query=The&page=2`
+    axios.get(url)
+        .then(
+            (result) => {
+                result.data.results.forEach(searchMovies => {
+                    let myMovie = new TrendMovies(searchMovies.id, searchMovies.title, searchMovies.release_date, searchMovies.poster_path, searchMovies.overview)
+                    listOfMovies.push(myMovie)
+                }
+                )
+                res.status(200).json(listOfMovies);
+            }
+        )
+        .catch((err) => { console.log(err) })
+}
+
+// popular page
+function popularHandler(req, res) {
+    let listOfMovies = [];
+    let url = `https://api.themoviedb.org/3/discover/movie?api_key=abb2fd3d9eb8c561dbf599a20e3ca490&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+    axios.get(url)
+        .then(
+            (result) => {
+                result.data.results.forEach(popularMovies => {
+                    let myMovie = new TrendMovies(popularMovies.id, popularMovies.original_title, popularMovies.release_date, popularMovies.poster_path, popularMovies.overview)
+                    listOfMovies.push(myMovie)
+                }
+                )
+                res.status(200).json(listOfMovies);
+            }
+        )
+        .catch((err) => { console.log(err) })
     }
-    return res.status(500).send(err);
-}
 
-app.listen(8080, ()=>{
-    console.log('listening to port 8080')
-})
+    // tv page
+    function tvDataHandler(req, res) {
+        let listOfMovies = [];
+        let url = `https://api.themoviedb.org/3/search/tv?api_key=abb2fd3d9eb8c561dbf599a20e3ca490&language=en-US&query=The&page=1`
+        axios.get(url)
+            .then(
+                (result) => {
+                    result.data.results.forEach(tvMovies => {
+                        let myMovie = new TvMovies(tvMovies.id, tvMovies.name, tvMovies.first_air_date, tvMovies.poster_path, tvMovies.overview)
+                        listOfMovies.push(myMovie)
+                    }
+                    )
+                    res.status(200).json(listOfMovies);
+                }
+            )
+            .catch((err) => { console.log(err) })
+    }
+
+
+
+    //errores
+    function notFoundHndler(req, res) {
+        return res.status(404).send("Not Found!!");
+    }
+    function serverErrorHndler(error, req, res) {
+        let err = {
+            status: 500,
+            message: "Sorry, something went wrong"
+        }
+        return res.status(500).send(err);
+    }
+
+    //listining to the port
+    app.listen(PORT, () => { console.log('listining') })
